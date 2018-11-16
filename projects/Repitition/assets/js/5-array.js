a9c94b5e28514e8ee2ace3f62cfabf46b9a8051c
Oskar Björk <Oskar.DTK.Bjork@lystkom.se>
2018-11-16 11:17:58 +0100

    arrays

--- projects/Repitition/assets/js/5-array.js
+++ projects/Repitition/assets/js/5-array.js
@@ -3,4 +3,7 @@ var friends1 = "Rickard";
 var friends2 = "Isak";
 
 //list a group of friend using an array
-var friend = ["Rickard", "Isak"]
+var friend = ["Rickard", "Isak", "emanuel", "Ted"]
+
+console.log(freinds[2] + " <3 " + freinds[3]);
+
