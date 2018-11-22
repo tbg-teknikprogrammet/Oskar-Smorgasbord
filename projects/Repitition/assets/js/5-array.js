d89a3e201c0a8e7082ed55e0718bdc7c03f34e3f
Oskar Björk <Oskar.DTK.Bjork@lystkom.se>
2018-11-22 14:06:05 +0100

    ForEach? and reverse

--- projects/Repitition/assets/js/5-array.js
+++ projects/Repitition/assets/js/5-array.js
@@ -3,7 +3,32 @@ var friends1 = "Rickard";
 var friends2 = "Isak";
 
 //list a group of friend using an array
-var friend = ["Rickard", "Isak", "emanuel", "Ted"]
+var friends = ["Rickard", "Isak", "emanuel", "Ted"];
 
-console.log(freinds[2] + " <3 " + freinds[3]);
+console.log(friends[2] + " <3 " + friends[3]);
+
+var array1 = [1,2,3,4,5,6];
+console.log(array1);
+var arrayreverse = array1.reverse(); 
+console.log(arrayreverse);
+
+Numbers_Same = [1,1,1,1,1,1,1,1,1,];
+Numbers_Not_Same = [1,2,3,4,5,6,7,8,9];
+var i = 0;
+Numbers_Same.forEach(Numbers_Same){
+    if(Numbers_Same[0] !== Numbers_Same[i])
+    {
+        return false;
+    }
+    i++;
+}
+Numbers_Not_Same.forEach(Numbers_Not_Same){
+    if(Numbers_Not.Same[0] !== Numbers_Not_Same[i])
+    {
+        return false;
+    }
+    i++;
+}
+   
+
 
