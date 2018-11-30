9e025a4d8e25c17361628ab9c7eb838b0d514857
Oskar Björk <Oskar.DTK.Bjork@lystkom.se>
2018-11-30 11:13:10 +0100

    added list

--- projects/Repitition/assets/js/5-array.js
+++ projects/Repitition/assets/js/5-array.js
@@ -32,7 +32,7 @@ console.log(arrayreverse);
 
 function isIdentical(array) {
     var first = array[0];
-    for(var i = 1; i < array.length; i++) {
+    for(var i = 0; i < array.length; i++) {
         if(array[i] !== first) {
             return false;
         }
--- projects/ToDo List/assets/js/ToDoList.js
+++ projects/ToDo List/assets/js/ToDoList.js
@@ -2,3 +2,5 @@
 //while
 //if
 //array med tillgörande metod
+document.getElementById("myBtn").addEventListener("click", clicked);
+function clicked(variable) 
