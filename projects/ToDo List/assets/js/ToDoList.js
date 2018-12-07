3f7bd5f2971e0721ef738fb5a94e2bbd0f8870a7
Oskar Björk <Oskar.DTK.Bjork@lystkom.se>
2018-12-07 10:14:27 +0100

    added add

--- projects/ToDo List/assets/js/ToDoList.js
+++ projects/ToDo List/assets/js/ToDoList.js
@@ -2,5 +2,40 @@
 //while
 //if
 //array med tillgörande metod
-document.getElementById("myBtn").addEventListener("click", clicked);
-function clicked(variable) 
+//document.getElementById("myBtn").addEventListener("click", clicked);
+var todolist =[];
+function clicked(input)
+{
+    input = prompt("hello this is alert");
+    var inputstr = input.split(" ");
+        switch (inputstr[0])
+    {
+        case "new":
+        {
+            var text = "";
+            for(var i = 1; i < inputstr.length; i++)
+            {
+                if(text = "")
+                {
+                    text = text + inputstr[i];
+                }
+                else
+                {
+                    text = text + " " + inputstr[i];
+                }
+                
+                console.log(inputstr[i]);
+            }
+            please = todolist.length;
+            todolist[please] = text;
+            console.log(please)
+            console.log(text)
+            console.log(inputstr)
+            console.log(todolist)
+        }
+        case "delete":
+        {
+
+        }
+    }    
+}
