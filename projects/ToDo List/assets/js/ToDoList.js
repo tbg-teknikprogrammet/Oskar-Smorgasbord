7270b7930df29888e6ccc97de3967353a0f77335
Oskar Björk <Oskar.DTK.Bjork@lystkom.se>
2018-12-13 13:10:22 +0100

    done and (hopfully) working

--- projects/ToDo List/assets/js/ToDoList.js
+++ projects/ToDo List/assets/js/ToDoList.js
@@ -4,6 +4,9 @@
 //array med tillgörande metod
 //document.getElementById("myBtn").addEventListener("click", clicked);
 var todolist =[];
+var todoindex = 0;
+var i = 0;
+var please = 0;
 function clicked(input)
 {
     input = prompt("hello this is alert");
@@ -14,28 +17,49 @@ function clicked(input)
         {
             var text = "";
             for(var i = 1; i < inputstr.length; i++)
-            {
-                if(text = "")
-                {
-                    text = text + inputstr[i];
-                }
-                else
-                {
-                    text = text + " " + inputstr[i];
-                }
-                
+            {       
+                text = text + inputstr[i] + " "; 
                 console.log(inputstr[i]);
             }
             please = todolist.length;
             todolist[please] = text;
+ 
             console.log(please)
-            console.log(text)
             console.log(inputstr)
             console.log(todolist)
         }
         case "delete":
         {
-
+            if(inputstr[1] > -1)
+            {
+                todolist.splice(inputstr[1],1);
+            }
+            console.log(inputstr);
+            console.log(todolist);
         }
+        removeElement();
+        addelements();
     }    
+}
+function addelements()
+{
+    console.log(todolist.length);
+    for(var i = 0; i < todolist.length; i++)
+    {
+        var ullist = document.getElementById("ullist")
+        var list = document.createElement("LI");
+        list.className = i;
+        list.setAttribute("id", "list")
+        list.innerHTML += i + " " + todolist[i];
+        ullist.appendChild(list);
+    }
+}
+function removeElement(element) 
+{
+    //Removes an element from the document
+    var element = document.getElementById("ullist")
+    console.log(element)
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
 }
