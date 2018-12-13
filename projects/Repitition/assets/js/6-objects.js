5b7e369d62ecde623ee707258d7aed6f21159190
Oskar Björk <Oskar.DTK.Bjork@lystkom.se>
2018-12-13 14:47:00 +0100

    object

---
+++ projects/Repitition/assets/js/6-objects.js
@@ -0,0 +1,11 @@
+var person = ["Sid", 31, "Malå"];
+console.log(person[2])
+var person2 = [31, "Livelong", "Bill"];
+console.log(person[2])
+var person = {
+    name: "Bob",
+    age: 29,
+    hometown: "Heresneezeduck"
+};
+console.log("thos person nem is "+person.name)
+var post = []
