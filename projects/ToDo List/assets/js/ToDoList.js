8db8a07550ed9788f3ce3a7ab27514d70bb87bde
Oskar Björk <Oskar.DTK.Bjork@lystkom.se>
2018-12-13 14:45:32 +0100

    fullständig kommentering

--- projects/ToDo List/assets/js/ToDoList.js
+++ projects/ToDo List/assets/js/ToDoList.js
@@ -5,61 +5,63 @@
 //document.getElementById("myBtn").addEventListener("click", clicked);
 var todolist =[];
 var todoindex = 0;
-var i = 0;
-var please = 0;
+var leng = 0;
 function clicked(input)
 {
     input = prompt("hello this is alert");
-    var inputstr = input.split(" ");
-        switch (inputstr[0])
+    var inputstr = input.split(" ");//tar input och konverterar till list/array
+        switch (inputstr[0])// specificerar första ordet för att ta red på kommandot
     {
         case "new":
         {
-            var text = "";
-            for(var i = 1; i < inputstr.length; i++)
+            var text = "";//resettar text efter varje gång
+            for(var i = 1; i < inputstr.length; i++)//konverterar alla ord efter kommandot blir till en lång string inom en arrray
             {       
-                text = text + inputstr[i] + " "; 
+                text = text + inputstr[i] + " "; //konverterar alla ord efter kommandot till string
                 console.log(inputstr[i]);
             }
-            please = todolist.length;
-            todolist[please] = text;
+            leng = todolist.length;//tog tre år för denna sak att fungera
+            todolist[leng] = text;
  
-            console.log(please)
-            console.log(inputstr)
-            console.log(todolist)
+            console.log(please)//debugging tools
+            console.log(inputstr)//debugging tools
+            console.log(todolist)//debugging tools
         }
         case "delete":
         {
-            if(inputstr[1] > -1)
+            if(inputstr[1] > -1)//it just works
             {
-                todolist.splice(inputstr[1],1);
+                todolist.splice(inputstr[1],1);//tar bort index och ä nice
             }
-            console.log(inputstr);
-            console.log(todolist);
+            console.log(inputstr);//debugging tools
+            console.log(todolist);//debugging tools
         }
-        removeElement();
-        addelements();
+        removeElement();//tillkallar function
+        addelements();//tillkallar function
+        //localStorage.setItem("names", JSON.stringify(todolist));
+        //var storedTodolist = JSON.parse(localStorage.getItem(names))
+        //test med local storage
     }    
 }
-function addelements()
+function addelements()//lägger till list elementet till html
 {
-    console.log(todolist.length);
-    for(var i = 0; i < todolist.length; i++)
+    console.log(todolist.length);//debuggint tool
+    for(var i = 0; i < todolist.length; i++)//lägger till alla list element
     {
-        var ullist = document.getElementById("ullist")
-        var list = document.createElement("LI");
+        var ullist = document.getElementById("ullist")//definerar ul
+        var list = document.createElement("LI");//skapar li
         list.className = i;
-        list.setAttribute("id", "list")
-        list.innerHTML += i + " " + todolist[i];
-        ullist.appendChild(list);
+        list.setAttribute("id", "list")//ger li id list
+        list.innerHTML += i + " " + todolist[i];//fyller innehållet på li med todolist
+        ullist.appendChild(list);//kopplar li som child till ul
     }
 }
-function removeElement(element) 
+function removeElement(element) //tar ner alla li i html för att js synkar inte med html
 {
     //Removes an element from the document
     var element = document.getElementById("ullist")
-    console.log(element)
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
+    console.log(element)//debug tool
+    while (element.firstChild) {//gör så alla bli bortagna
+        element.removeChild(element.firstChild);//tarbort elementet
     }
 }
