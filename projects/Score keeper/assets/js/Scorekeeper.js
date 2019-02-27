07bdaf22cfe8e4f666709c9374461ec386674cad
Oskar Björk <Oskar.DTK.Bjork@lystkom.se>
2019-02-27 11:10:14 +0100

    Done

--- projects/Score keeper/assets/js/Scorekeeper.js
+++ projects/Score keeper/assets/js/Scorekeeper.js
@@ -7,72 +7,93 @@ var test = 0;
 inputscore.addEventListener("click", function(){
     Data.maxscore = document.getElementById("scoremax").value;
     test = document.getElementById("scoremax").value;
+    updatElementScore();
     console.log(test)
 });    
 
 addelementsH1();
+addelementsScore();
 
 function clickedP1()
 {
-    Data.player1++;
-    if(Data.player1 <= Data.maxscore)
+    console.log(Data.winner)
+    if(Data.winner == 0)
     {
-        removeElementH1();//tillkallar function
-        addelementsH1();//tillkallar function
+        Data.player1++;
+    }
+    if(Data.player1 < Data.maxscore)
+    {   
+        updatElementH1();
     }
     else
     {
         Data.winner = 1;
-        removeElementP();
-        addelementsP();
+        updatElementH1();
+        updatElementP();
     }
-    
 }
 function clickedP2()
 {
-    Data.player2++;
-    if(Data.player2 <= Data.maxscore)
+    console.log(Data.winner)
+    if(Data.winner == 0)
     {
-        removeElementH1();//tillkallar function
-        addelementsH1();//tillkallar function
+        Data.player2++;
+    }
+    if(Data.player2 < Data.maxscore)
+    {
+        updatElementH1();
     }
     else
     {
         Data.winner = 2;
-        removeElementP();
-        addelementsP();
+        updatElementH1();
+        updatElementP();
     }
 }
 function clickedR()
 {
+    console.log(Data)
     Data.player1 = 0;
-    Data.Player2 = 0;
-    Data.Winner = 0;
+    Data.player2 = 0;
+    Data.winner = 0;
 
-    removeElementH1();
-    addelementsH1();
-
+    updatElementH1();
     removeElementP();
 }
 function addelementsH1()//lägger till list elementet till html
 {
-    
     var ullist = document.getElementById("scorekeeper")//definerar ul
-    var H1 = document.createElement("H1");//skapar H1
-    H1.setAttribute("id", "H1")//ger li id list
-    H1.innerHTML += Data.player1 + "to" + Data.player2;//fyller innehållet på li med todolist
-    ullist.appendChild(H1);//kopplar li som child till ul
-
+    var h1 = document.createElement("h1");//skapar H1
+    h1.setAttribute("id", "h1")//ger h1 id list
+    if(Data.winner == 1)
+    {
+        h1.innerHTML = "<span style=\"color:green\">"+ Data.player1 + "</span>" + " to " + Data.player2;
+    }
+    else if(Data.winner == 2)
+    {
+        h1.innerHTML = Data.player1 + " to " + "<span style=\"color:green\">" + Data.player2 + "</span>";
+    }
+    else
+    {
+        h1.innerHTML = Data.player1 + " to " + Data.player2;//fyller innehållet på li med todolist
+    }
+    ullist.appendChild(h1);//kopplar li som child till ul
 }
 function addelementsP(winner)//lägger till list elementet till html
-{
-    
+{   
     var ullist = document.getElementById("ullist")//definerar ul
-    var p = document.createElement("p");//skapar H1
+    var p = document.createElement("h1");//skapar H1
     p.setAttribute("id", "p")//ger li id list
     p.innerHTML += "The winner is player" + Data.winner;//fyller innehållet på li med todolist
     ullist.appendChild(p);//kopplar li som child till ul
-
+}
+function addelementsScore()//lägger till list elementet till html
+{   
+    var ullist = document.getElementById("score")//definerar ul
+    var p = document.createElement("h1");//skapar H1
+    p.setAttribute("id", "s")//ger li id list
+    p.innerHTML += "First to  " + Data.maxscore;//fyller innehållet på li med todolist
+    ullist.appendChild(p);//kopplar li som child till ul
 }
 function removeElementH1(element) //tar ner alla li i html för att js synkar inte med html
 {
@@ -91,4 +112,28 @@ function removeElementP(element) //tar ner alla li i html för att js synkar int
     while (element.firstChild) {//gör så alla bli bortagna
         element.removeChild(element.firstChild);//tarbort elementet
     }
+}
+function removeElementScore(element) //tar ner alla li i html för att js synkar inte med html
+{
+    //Removes an element from the document
+    var element = document.getElementById("score")
+    console.log(element)//debug tool
+    while (element.firstChild) {//gör så alla bli bortagna
+        element.removeChild(element.firstChild);//tarbort elementet
+    }
+}
+function updatElementH1()
+{
+    removeElementH1();
+    addelementsH1();
+}
+function updatElementP()
+{
+    removeElementP();
+    addelementsP();
+}
+function updatElementScore()
+{
+    removeElementScore();
+    addelementsScore();
 }
